781932031f35fa9d7bb8047c2300780c7aa2ad08
dongeun <kin911014@daum.net>
2020-02-24 12:09:42 +0900

    remind guestbook03 finish

--- re-guestbook03/src/main/java/com/douzone/reguestbook/controller/ReGuestbookController.java
+++ re-guestbook03/src/main/java/com/douzone/reguestbook/controller/ReGuestbookController.java
@@ -2,8 +2,13 @@ package com.douzone.reguestbook.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.douzone.reguestbook.repository.GuestbookRepository;
 import com.douzone.reguestbook.vo.GuestbookVo;
@@ -11,12 +16,35 @@ import com.douzone.reguestbook.vo.GuestbookVo;
 @Controller
 public class ReGuestbookController {
 	
+	// Repository에서 어노테이션을 안해줄 경우 injection에러가 난다.
+	@Autowired
 	private GuestbookRepository guestbookRepository;
 	
-	@RequestMapping("/")
-	public String index() {
+	@RequestMapping(value="/", method=RequestMethod.GET)
+	public String index(Model model) {
 		List<GuestbookVo> list = guestbookRepository.findAll();
-		return "hello guestbook003";
+		model.addAttribute("list", list);
+		return "index";
+	}
+	
+	@RequestMapping(value="/add", method=RequestMethod.POST)
+	public String index(GuestbookVo vo) {
+		System.out.println(vo);
+		guestbookRepository.insert(vo);
+		return "redirect:/";
+	}
+	
+	@RequestMapping(value="/deleteform/{no}", method=RequestMethod.GET)
+	public String deleteform(@PathVariable("no") Long no, Model model) {
+		model.addAttribute("no", no);
+		return "deleteform";
+	}
+	
+	@RequestMapping(value="/delete/{no}", method=RequestMethod.POST)
+	public String delete(@PathVariable("no") Long no, 
+			@RequestParam(value="password", required=true, defaultValue="")  String password) {
+		guestbookRepository.delete(no, password);
+		return "redirect:/";
 	}
 	
 }
---
+++ re-guestbook03/src/main/webapp/WEB-INF/views/remove.jsp
@@ -0,0 +1,29 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%
+	/* request.setCharacterEncoding("UTF-8");
+	String noSt = request.getParameter("no");
+	Long no = Long.parseLong(noSt);
+	String password = request.getParameter("password");
+	
+	GuestbookVo vo = new GuestbookVo();
+	vo.setNo(no);
+	vo.setPassword(password);
+	if(new GuestbookRepository().ck(vo)){
+	new GuestbookRepository().delete(vo);	
+	}
+	response.sendRedirect("/guestbook01"); */
+%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+</body>
+</html>
